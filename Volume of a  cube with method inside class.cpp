849e5c7672cd95d949aa649332f87535fb40e8b7
65NinadA3 <ninadphatak123@gmail.com>
2025-08-17 09:35:38 +0530

    Create Volume of a  cube with method inside class.cpp

---
+++ Volume of a  cube with method inside class.cpp
@@ -0,0 +1,22 @@
+//Name : Ninad Phatak
+//PRN : 24070123065
+//Batch : ENTC A3
+#include <iostream>
+using namespace std;
+class Cube {
+float side;
+public:
+    void input() {
+        cout << "Enter side of cube: ";
+        cin >> side;
+    }
+    float volume() {
+        return side * side * side;
+    }
+};
+int main() {
+    Cube c;
+    c.input();
+    cout <<"Volume: "<<c.volume()<<endl;
+    return 0;
+}
